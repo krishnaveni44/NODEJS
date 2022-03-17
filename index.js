2abb59fd04c24b1ecabef6eeff8948fa12831058
krishnaveni44 <krishnaveni44@gmail.com>
2022-03-17 10:43:52 +0530

    API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // const express = require('express'); // "type": "commend in package.json"
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";  // "type": "module",
 import { MongoClient } from "mongodb";
@@ -20,10 +21,13 @@ const movies = [
 {"id":"101",
 "name":"No Country for Old Men",
 "poster":"https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-"rating":8.1,"summary":"A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+"rating":8.1,
+"summary":"A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
 "trailer":"https://www.youtube.com/embed/38A__WT3-o0"},
 
-{"id":"102","name":"Jai Bhim","poster":"https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
+{"id":"102",
+"name":"Jai Bhim",
+"poster":"https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
 "summary":"A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
 "rating":8.8,
 "trailer":"https://www.youtube.com/embed/nnXpbTFrqXA"},
@@ -34,13 +38,15 @@ const movies = [
 "poster":"https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
 "trailer":"https://www.youtube.com/embed/eOrNdBpGMv8"},
 
-{"id":"104","name":"Interstellar",
+{"id":"104",
+"name":"Interstellar",
 "poster":"https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
 "rating":8.6,
 "summary":"When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
 "trailer":"https://www.youtube.com/embed/zSWdZVtXT7E"},
 
-{"id":"105","name":"Baahubali",
+{"id":"105",
+"name":"Baahubali",
 "poster":"https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
 "rating":8,
 "summary":"In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
@@ -53,6 +59,8 @@ const movies = [
 "summary":"Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
 "trailer":"https://www.youtube.com/embed/NgsQ8mVkN8w"}
 ];
+
+app.use(cors());
 // middle ware -> Intercept -> converting body to json
 app.use(express.json());
 
